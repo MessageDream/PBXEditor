17f83a0fba8e8dd4f120ae97bf9b07d4f8ea44e8
jayden <jaydenzhao@outlook.com>
2015-09-25 00:16:02 +0800

    dictionary

--- PBXEditor/PBXFileReference.swift
+++ PBXEditor/PBXFileReference.swift
@@ -19,4 +19,58 @@ public class PBXFileReference:PBXObject{
     public var compilerFlags:String?
     public var buildPhase:String?
     
+    public static let typeNames = [
+        ".a":"archive.ar",
+        ".app": "wrapper.application",
+        ".s": "sourcecode.asm",
+        ".c": "sourcecode.c.c",
+        ".cpp": "sourcecode.cpp.cpp",
+        ".framework": "wrapper.framework",
+        ".h": "sourcecode.c.h",
+        ".pch": "sourcecode.c.h",
+        ".icns": "image.icns",
+        ".m":"sourcecode.c.objc",
+        ".mm":"sourcecode.cpp.objcpp",
+        ".nib":"wrapper.nib",
+        ".plist":"text.plist.xml",
+        ".png":"image.png",
+        ".rtf":"text.rtf",
+        ".tiff":"image.tiff",
+        ".txt":"text",
+        ".xcodeproj":"wrapper.pb-project",
+        ".xib":"file.xib",
+        ".strings":"text.plist.strings",
+        ".bundle":"wrapper.plug-in",
+        ".dylib":"compiled.mach-o.dylib",
+        ".json":"text.json"
+    ]
+    
+    public static let typePhases = [
+        ".a": "PBXFrameworksBuildPhase",
+        ".app": nil,
+        ".s": "PBXSourcesBuildPhase",
+        ".c": "PBXSourcesBuildPhase",
+        ".cpp": "PBXSourcesBuildPhase",
+        ".framework": "PBXFrameworksBuildPhase",
+        ".h": nil,
+        ".pch": nil,
+        ".icns": "PBXResourcesBuildPhase",
+        ".m": "PBXSourcesBuildPhase",
+        ".mm": "PBXSourcesBuildPhase",
+        ".nib": "PBXResourcesBuildPhase",
+        ".plist": "PBXResourcesBuildPhase",
+        ".png": "PBXResourcesBuildPhase",
+        ".rtf": "PBXResourcesBuildPhase",
+        ".tiff": "PBXResourcesBuildPhase",
+        ".txt": "PBXResourcesBuildPhase",
+        ".json": "PBXResourcesBuildPhase",
+        ".xcodeproj": nil,
+        ".xib": "PBXResourcesBuildPhase",
+        ".strings": "PBXResourcesBuildPhase",
+        ".bundle": "PBXResourcesBuildPhase",
+        ".dylib": "PBXFrameworksBuildPhase"
+    ]
+    
+
+    
 }
